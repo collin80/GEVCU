dc1a4043009b65fae756ecb1ef0952b5d25e4686
Collin Kidder <collink@kkmfg.com>
2013-03-24 16:40:42 -0400

    Added a preference handler class
    The new PREFHANDLER class abstracts away all the details of how
    and where values are stored in EEPROM and transparently
    allows everything to be retargetted to last known good location
    on demand.

---
+++ pref_handler.h
@@ -0,0 +1,41 @@
+/*
+ * pref_handler.h
+ *
+ * header for preference handler
+ *
+ * Created: 3/24/2013 
+ *  Author: Collin Kidder
+ */ 
+
+
+#ifndef PREF_HANDLER_H_
+#define PREF_HANDLER_H_
+
+#include <Arduino.h>
+
+#define PREF_MODE_NORMAL  false
+#define PREF_MODE_LKG     true
+
+class PREFHANDLER {
+  private:
+  uint32_t base_address; //base address for the parent device
+  uint32_t lkg_address;
+  bool use_lkg; //use last known good config?
+  
+  public:
+  PREFHANDLER();
+  PREFHANDLER(uint32_t base);
+  void LKG_mode(bool mode);
+  void Write(uint16_t address, uint8_t val);
+  void Write(uint16_t address, uint16_t val);
+  void Write(uint16_t address, uint32_t val);
+  void Read(uint16_t address, uint8_t *val);
+  void Read(uint16_t address, uint16_t *val);
+  void Read(uint16_t address, uint32_t *val);
+  uint8_t calcChecksum();
+  void saveChecksum();
+  bool checksumValid();
+  
+};
+	
+#endif
