931d8dc1d0da035e2af232e23b798ea737daa5a5
Collin Kidder <collink@kkmfg.com>
2013-07-24 16:38:21 -0400

    Updated licensing terms in the readme and all source/header files. Hurray for lawyers.

--- GEVCU.ino
+++ GEVCU.ino
@@ -17,6 +17,28 @@
  The system automatically manages when to call the tick handlers and automatically filters canbus and sends frames to the devices.
  There is a facility to send data between devices by targetting a certain type of device. For instance, a motor controller
  can ask for any throttles and then retrieve the current throttle position from them.
+
+Copyright (c) 2013 Collin Kidder, Michael Neuweiler, Charles Galpin
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
  */
 
 #include "GEVCU.h"
