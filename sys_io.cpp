91ec6ea6dbbeeb533a7af77cab7c52be47e791e1
Collin Kidder <collink@kkmfg.com>
2013-07-31 21:03:08 -0400

    Changed the order of digital inputs
    The older DueD board had out of order digital inputs
    but the new one has them all in order. So this change
    makes the digital inputs match the pinout of the AmpSeal
    connector.

--- sys_io.cpp
+++ sys_io.cpp
@@ -37,7 +37,7 @@ by stimmer
 
 //pin definitions for system IO
 uint8_t adc[NUM_ANALOG][2] = {{1,0}, {2,3}, {4,5}, {7,6}}; //low, high
-uint8_t dig[] = {11, 9, 13, 12};
+uint8_t dig[] = {9, 11, 12, 13};
 uint8_t out[] = {55, 22, 48, 34};
 
 volatile int bufn,obufn;
