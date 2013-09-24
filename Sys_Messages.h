bfdfa5d99516327fbf3e39b7ed3b687c820fad81
Charles Galpin <cgalpin@lhsw.com>
2013-09-24 17:20:42 -0400

    sync with Collin's Arduino branch

---
+++ Sys_Messages.h
@@ -0,0 +1,38 @@
+/*
+ * Sys_Messages.h
+ *
+Copyright (c) 2013 Collin Kidder, Michael Neuweiler, Charles Galpin
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef SYSMSG_H_
+#define SYSMSG_H_
+
+
+enum SystemMessage {
+	MSG_STARTUP = 0x3000,
+	MSG_SOFT_FAULT = 0x3100,
+	MSG_HARD_FAULT = 0x3150,
+	MSG_DISABLE = 0x3200,
+	MSG_ENABLE = 0x3300
+};
+
+#endif
