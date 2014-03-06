6c6a57bb3df766dda4aeab3644e28ccf1fe31047
Collin Kidder <collink@kkmfg.com>
2014-03-06 15:33:37 -0500

    Fixed config.h to properly default to using SerialUSB. 95% of everyone is going to need that default now.

--- config.h
+++ config.h
@@ -43,7 +43,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * SERIAL CONFIGURATION
  */
 #define CFG_SERIAL_SPEED 115200
-#define SerialUSB Serial // re-route serial-usb output to programming port ;) comment if output should go to std usb
+//#define SerialUSB Serial // re-route serial-usb output to programming port ;) comment if output should go to std usb
 
 
 //The defines that used to be here to configure devices are gone now.
