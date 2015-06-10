c6298a52c3a585b173b3e04efa207090a3a80054
Michael Neuweiler <michael@neuwiler.biz>
2015-06-10 21:04:32 +0200

    filter ECU responses not related to throttle position to prevent unwanted movement

--- CanThrottle.cpp
+++ CanThrottle.cpp
@@ -67,7 +67,7 @@ void CanThrottle::setup()
             break;
 
         case Volvo_V50_Diesel:
-            // Request: dlc=0x08 fid=0xFFFFE id=0x3FFFE ide=0x01 rtr=0x00 data=0xCD,0x11,0xA6,0x00,0x24,0x01,0x00,0x00 ([0x00, 0xf, 0xff, 0xfe, 0xcd, 0x11, 0xa6, 0x00, 0x24, 0x01, 0x00, 0x00])
+            // Request: dlc=0x08 fid=0xFFFFE id=0x3FFFE ide=0x01 rtr=0x00 data=0xCD,0x11,0xA6,0x00,0x24,0x01,0x00,0x00 (vida: [0x00, 0xf, 0xff, 0xfe, 0xcd, 0x11, 0xa6, 0x00, 0x24, 0x01, 0x00, 0x00])
             // Response: dlc=0x08 fid=0x400021 id=0x21 ide=0x01 rtr=0x00 data=0xCE,0x11,0xE6,0x00,0x24,0x03,0xFD,0x00 (vida: [0x00, 0x40, 0x00, 0x21, 0xce, 0x11, 0xe6, 0x00, 0x24, 0x03, 0xfd, 0x00])
             requestFrame.id = 0x3FFFE;
             requestFrame.extended = 0x01;
@@ -123,14 +123,21 @@ void CanThrottle::handleCanFrame(CAN_FRAME *frame)
     if (frame->id == responseId) {
         switch (config->carType) {
             case Volvo_S80_Gas:
-                rawSignal.input1 = frame->data.bytes[4];
+                // only evaluate messages with payload 0x04,0x62,0xEE,0xCB as other ECU data is also sent by with 0x738 - this must not cause the motor to spin up (e.g. diagnostic software)
+                if (frame->data.bytes[0] == 0x04 && frame->data.bytes[1] == 0x62 && frame->data.bytes[2] == 0xee && frame->data.bytes[3] == 0xcb) {
+                    rawSignal.input1 = frame->data.bytes[4];
+                    ticksNoResponse = 0;
+                }
                 break;
 
             case Volvo_V50_Diesel:
-                rawSignal.input1 = (frame->data.bytes[5] + 1) * frame->data.bytes[6];
+                // only evaluate messages with payload 0xCE,0x11,0xE6,0x00,0x2
+                if (frame->data.bytes[0] == 0xce && frame->data.bytes[1] == 0x11 && frame->data.bytes[2] == 0x6E && frame->data.bytes[3] == 0x00 && frame->data.bytes[4] == 0x02) {
+                    rawSignal.input1 = (frame->data.bytes[5] + 1) * frame->data.bytes[6];
+                    ticksNoResponse = 0;
+                }
                 break;
         }
-        ticksNoResponse = 0;
     }
 }
 
